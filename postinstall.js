f27a215dd7833c13b0283b376aef303a9a9f8031
Avaer Kazmer <a@modules.io>
2018-04-28 01:20:37 -0400

    Make postinstall.js idempotent

--- postinstall.js
+++ postinstall.js
@@ -3,50 +3,58 @@ const fs = require('fs');
 const os = require('os');
 const unzpr = require('unzpr');
 const rimraf = require('rimraf');
-const ws = fs.createReadStream(path.join(__dirname, 'lib.zip'))
-  .pipe(unzpr.Extract({
+const rs = fs.createReadStream(path.join(__dirname, 'lib.zip'));
+rs.on('ready', () => {
+  const ws = rs.pipe(unzpr.Extract({
     path: __dirname,
   }));
-
-ws.on('done', () => {
-  rimraf(path.join(__dirname, 'lib.zip'), err => {
-    if (err) {
-      throw err;
+  ws.on('done', () => {
+    rimraf(path.join(__dirname, 'lib.zip'), err => {
+      if (err) {
+        throw err;
+      }
+    });
+    const platform = os.platform();
+    switch (platform) {
+      case 'win32': {
+        ['macos', 'linux', 'android', 'ios'].forEach(p => {
+          rimraf(path.join(__dirname, 'lib', p), err => {
+            if (err) {
+              throw err;
+            }
+          });
+        });
+        break;
+      }
+      case 'darwin': {
+        ['windows', 'linux', 'android', 'ios'].forEach(p => {
+          rimraf(path.join(__dirname, 'lib', p), err => {
+            if (err) {
+              throw err;
+            }
+          });
+        });
+        break;
+      }
+      case 'linux': {
+        ['windows', 'macos', 'android', 'ios'].forEach(p => {
+          rimraf(path.join(__dirname, 'lib', p), err => {
+            if (err) {
+              throw err;
+            }
+          });
+        });
+        break;
+      }
+      default: throw new Error('unknown platform: ' + platform);
     }
   });
-  const platform = os.platform();
-  switch (platform) {
-    case 'win32': {
-      ['macos', 'linux', 'android', 'ios'].forEach(p => {
-        rimraf(path.join(__dirname, 'lib', p), err => {
-          if (err) {
-            throw err;
-          }
-        });
-      });
-      break;
-    }
-    case 'darwin': {
-      ['windows', 'linux', 'android', 'ios'].forEach(p => {
-        rimraf(path.join(__dirname, 'lib', p), err => {
-          if (err) {
-            throw err;
-          }
-        });
-      });
-      break;
-    }
-    case 'linux': {
-      ['windows', 'macos', 'android', 'ios'].forEach(p => {
-        rimraf(path.join(__dirname, 'lib', p), err => {
-          if (err) {
-            throw err;
-          }
-        });
-      });
-      break;
-    }
-    default: throw new Error('unknown platform: ' + platform);
+});
+rs.on('error', err => {
+  if (err.code === 'ENOENT') {
+    process.exit(0);
+  } else {
+    throw err;
   }
 });
 
